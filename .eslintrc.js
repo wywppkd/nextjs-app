51e6b021071a0887b534a288eb1dae50a393040d
Bran <1055025755@qq.com>
2022-06-07 15:00:22 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  rules: {
+    'no-console': 'off',
+    'react-hooks/exhaustive-deps': 'error', // react-hooks 依赖检查
+    'no-empty': 'off', // catch{} 允许为空
+    '@typescript-eslint/no-shadow': ['off'], // 当前作用域变量名不能与父级作用域变量同名
+  },
+};
